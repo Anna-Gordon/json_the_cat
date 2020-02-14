255917e7dfa539824a70a06b47a1d343c06a1cf3
Anna Gordon <annetta@email.com>
2020-02-14 00:18:41 +0000

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
-const userInput = process.argv.slice(2).toString();
-const query = userInput.slice(0, 3).toLowerCase();
-let url = "https://api.thecatapi.com/v1/breeds/search?q=" + query;
 
-request(url, function(error, response, body) {
-  if (body.length === 2) {
-    console.log("Breed Not Found");
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-  }
 
-  if (!error) {
-  } else {
-    console.log(error);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  breedName = breedName.slice(0, 3).toLowerCase();
+  let url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(url, function(error, response, body) {
+    let data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback(error, null);
+    }
+    let descData = data[0]["description"];
+    return callback(null, descData);
+    
+  });
+};
 
-});
+module.exports = { fetchBreedDescription };
+
